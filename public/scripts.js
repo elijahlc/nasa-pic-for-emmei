cc91b4514c45bbef7593ca7cae5537fafe1af6ab
Eli Cohen <cohen.elijahlev@gmail.com>
2022-08-12 07:50:07 -0400

    mvp

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,7 @@
 const button = document.querySelector('button');
 const div = document.querySelector('div');
 
-const randDate = () => {
-	console.log(new Date());
-};
-
 button.addEventListener('click', async () => {
-	randDate();
-
 	const config = {
 		params: { api_key: 'gYwfDAePsJCg6hg52CFQbZ21jXCCcv4zJNGAei6T' },
 	};
@@ -22,7 +16,19 @@ button.addEventListener('click', async () => {
 	const img = document.createElement('IMG');
 	img.src = url;
 
-	div.appendChild(img);
+	const h2 = document.createElement('h2');
+	h2.innerHTML = title;
 
-	console.log(response);
+	const h3 = document.createElement('h3');
+	h3.innerHTML = `Daily image on ${date.split('-')[1]}/${date.split('-')[2]}/${
+		date.split('-')[0]
+	}`;
+
+	const p = document.createElement('p');
+	p.innerHTML = explanation;
+
+	div.appendChild(img);
+	div.appendChild(h2);
+	div.appendChild(h3);
+	div.appendChild(p);
 });

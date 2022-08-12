4d144cb2f4ebebd6728289aa14170483b6b0f8fa
Eli Cohen <cohen.elijahlev@gmail.com>
2022-08-12 08:21:59 -0400

    added random functionality

--- public/scripts.js
+++ public/scripts.js
@@ -2,33 +2,86 @@ const button = document.querySelector('button');
 const div = document.querySelector('div');
 
 button.addEventListener('click', async () => {
-	const config = {
-		params: { api_key: 'gYwfDAePsJCg6hg52CFQbZ21jXCCcv4zJNGAei6T' },
-	};
+	try {
+		const config = {
+			params: {
+				api_key: 'gYwfDAePsJCg6hg52CFQbZ21jXCCcv4zJNGAei6T',
+				date: `${randomDate()}`,
+			},
+		};
 
-	const response = await axios.get(
-		'https://api.nasa.gov/planetary/apod',
-		config
-	);
+		const response = await axios.get(
+			'https://api.nasa.gov/planetary/apod',
+			config
+		);
 
-	const { url, explanation, date, title } = response.data;
+		const { url, explanation, date, title } = response.data;
 
-	const img = document.createElement('IMG');
-	img.src = url;
+		const img = document.querySelector('img');
+		const h2 = document.querySelector('h2');
+		const h3 = document.querySelector('h3');
+		const p = document.querySelector('p');
 
-	const h2 = document.createElement('h2');
-	h2.innerHTML = title;
-
-	const h3 = document.createElement('h3');
-	h3.innerHTML = `Daily image on ${date.split('-')[1]}/${date.split('-')[2]}/${
-		date.split('-')[0]
-	}`;
-
-	const p = document.createElement('p');
-	p.innerHTML = explanation;
-
-	div.appendChild(img);
-	div.appendChild(h2);
-	div.appendChild(h3);
-	div.appendChild(p);
+		img.src = url;
+		h2.innerHTML = title;
+		h3.innerHTML = `Daily image on ${date.split('-')[1]}/${
+			date.split('-')[2]
+		}/${date.split('-')[0]}`;
+		p.innerHTML = explanation;
+	} catch (err) {
+		console.log(err);
+	}
 });
+
+function randomDate() {
+	const days = [
+		'01',
+		'02',
+		'03',
+		'04',
+		'05',
+		'06',
+		'07',
+		'08',
+		'09',
+		'10',
+		'11',
+		'12',
+		'13',
+		'14',
+		'15',
+		'16',
+		'17',
+		'18',
+		'19',
+		'20',
+		'21',
+		'22',
+		'23',
+		'24',
+		'25',
+		'26',
+		'27',
+		'28',
+	];
+
+	const months = [
+		'01',
+		'02',
+		'03',
+		'04',
+		'05',
+		'06',
+		'07',
+		'08',
+		'09',
+		'10',
+		'11',
+		'12',
+	];
+
+	const month = months[Math.floor(Math.random() * 12)];
+	const day = days[Math.floor(Math.random() * 28)];
+	const year = Math.floor(Math.random() * 22 + 2000).toString();
+	return `${year}-${month}-${day}`;
+}
